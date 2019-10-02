0b4b67f60664116f82a2d5d8a5a4613b3596d960
erik <erikrm@stud.ntnu.no>
2019-10-02 10:16:49 +0200

    Moved these two files to test_files due to them not being needed anymore

--- choose_file_example.py
+++
@@ -1,6 +0,0 @@
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename(initialdir='./', title = "Select flight", filetypes = (("pcap files","*.pcap"),("all files","*.*"))) # show an "Open" dialog box and return the path to the selected file
-print(filename)
